fd602e3f74e1bce44f5acb3a8c3301499eb2f39b
Gabriel Pivato <pivatogabriel@gmail.com>
2018-06-08 01:53:39 +0000

    update

--- shell_scripts/software.sh
+++ shell_scripts/software.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #
-sudo apt-get vim
+sudo apt-get install vim
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install npm
+npm install onoff
